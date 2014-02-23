afa54cf53b4a536dffb7a7152a46b464bc124a4e
d <d.g.feat.th2iinchoiincho@gmail.com>
2014-02-23 17:18:19 +0900

    行き詰まった項の扱いを変更

--- Tapl.hs
+++ Tapl.hs
@@ -1,19 +1,16 @@
-import Control.Monad
-import Data.List
+import Control.Applicative
 
 class System t where
     isValue :: t -> Bool
-    evalStep :: t -> t
+    evalStep :: t -> Maybe t
 
-eval :: System t => t -> t
-eval t = if isValue t then t else eval $ evalStep t
-
-evalProgress :: System t => t -> [t]
-evalProgress t = (flip unfoldr) (Just t)
-                 $ fmap
-                 $ \t'->(t', if isValue t'
-                             then Nothing
-                             else Just $ evalStep t')
+evalProgress :: System t => t -> (Bool, [t])
+evalProgress t = f t
+    where f t = if isValue t
+                then (True, [])
+                else case evalStep t of
+                       Just t' -> let (b, ts) = evalProgress t' in (b, t':ts)
+                       Nothing -> (False, [])
 
 data Term = TTrue
           | TFalse
@@ -26,6 +23,7 @@ data Term = TTrue
           | TIndex Int
           | TApply Term Term
             deriving (Show,Eq)
+
 instance System Term where
     isValue TTrue = True
     isValue TFalse = True
@@ -34,19 +32,20 @@ instance System Term where
     isValue t@(TPred _) = isIntValue t
     isValue (TAbs _) = True
     isValue _ = False
-    evalStep (TIf TTrue t _) = t
-    evalStep (TIf TFalse _ f) = f
-    evalStep (TIf c t f) = TIf (evalStep c) t f
-    evalStep (TIsZero TZero) = TTrue
-    evalStep (TIsZero n) | isIntValue n = TFalse
-                         | otherwise = TIsZero $ evalStep n
-    evalStep (TSucc (TPred n)) | isIntValue n = n
-    evalStep (TPred (TSucc n)) | isIntValue n = n
-    evalStep (TSucc n) | not $ isIntValue n = TSucc $ evalStep n
-    evalStep (TPred n) | not $ isIntValue n = TPred $ evalStep n
-    evalStep (TApply t1 t2) | not $ isValue t1 = TApply (evalStep t1) t2
-                            | not $ isValue t2 = TApply t1 $ evalStep t2
-                            | otherwise = case t1 of (TAbs t') -> substitute t' t2
+    evalStep (TIf TTrue t _) = Just t
+    evalStep (TIf TFalse _ f) = Just f
+    evalStep (TIf c t f) = TIf <$> evalStep c <*> pure t <*> pure f
+    evalStep (TIsZero TZero) = Just TTrue
+    evalStep (TIsZero n) | isIntValue n = Just TFalse
+                         | otherwise = TIsZero <$> evalStep n
+    evalStep (TSucc (TPred n)) | isIntValue n = Just n
+    evalStep (TPred (TSucc n)) | isIntValue n = Just n
+    evalStep (TSucc n) | not $ isValue n = TSucc <$> evalStep n
+    evalStep (TPred n) | not $ isValue n = TPred <$> evalStep n
+    evalStep (TApply t1 t2) | not $ isValue t1 = TApply <$> evalStep t1 <*> pure t2
+                            | not $ isValue t2 = TApply t1 <$> evalStep t2
+                            | otherwise = case t1 of (TAbs t') -> Just $ substitute t' t2
+    evalStep _ = Nothing
 
 isIntValue :: Term -> Bool
 isIntValue TZero = True
@@ -73,32 +72,35 @@ test actual expected = if actual == expected
                        else error $ concat [ "failed\nExpected: "
                                            , show expected
                                            , "\nActual: "
-                                           , show actual]
+                                           , show actual
+                                           ]
 
 main :: IO ()
-main = forM_ (zipWith (\x y->(x, x:y)) term expected) $ \(x, y) -> test (evalProgress x) y
+main = sequence_ $ zipWith test (map evalProgress term) expected
     where term = [ TIf (TIf TTrue (TIf TFalse TTrue TFalse) TTrue)
                        (TSucc $ TSucc $ TPred TZero) $
                        TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
                  , TApply (TApply (TApply tst t) TTrue) $ TPred $ TSucc TZero
                  ]
-          expected = [ [ TIf (TIf TFalse TTrue TFalse)
-                             (TSucc $ TSucc $ TPred TZero) $
-                             TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
-                       , TIf TFalse
-                             (TSucc $ TSucc $ TPred TZero) $
-                             TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
-                       , TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
-                       , TSucc $ TPred TZero
-                       , TZero
-                       ]
-                     , [ TApply (TApply (TAbs $ TAbs $ TApply (TApply t $ TIndex 2) $ TIndex 1) TTrue) $ TPred $ TSucc TZero
-                       , TApply (TAbs $ TApply (TApply t TTrue) $ TIndex 1) $ TPred $ TSucc TZero
-                       , TApply (TAbs $ TApply (TApply t TTrue) $ TIndex 1) $ TZero
-                       , TApply (TApply t TTrue) TZero
-                       , TApply (TAbs TTrue) TZero
-                       , TTrue
-                       ]
+          expected = [ (True,
+                        [ TIf (TIf TFalse TTrue TFalse)
+                              (TSucc $ TSucc $ TPred TZero) $
+                              TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
+                        , TIf TFalse
+                              (TSucc $ TSucc $ TPred TZero) $
+                              TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
+                        , TApply (TAbs $ TSucc $ TIndex 1) $ TPred TZero
+                        , TSucc $ TPred TZero
+                        , TZero
+                        ])
+                     , (True,
+                        [ TApply (TApply (TAbs $ TAbs $ TApply (TApply t $ TIndex 2) $ TIndex 1) TTrue) $ TPred $ TSucc TZero
+                        , TApply (TAbs $ TApply (TApply t TTrue) $ TIndex 1) $ TPred $ TSucc TZero
+                        , TApply (TAbs $ TApply (TApply t TTrue) $ TIndex 1) $ TZero
+                        , TApply (TApply t TTrue) TZero
+                        , TApply (TAbs TTrue) TZero
+                        , TTrue
+                        ])
                      ]
 
 t = TAbs $ TAbs $ TIndex 2

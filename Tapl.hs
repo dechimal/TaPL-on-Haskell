72df0b051942e9343653971c11cdbbbba5898945
d <d.g.feat.th2iinchoiincho@gmail.com>
2014-10-08 19:07:15 +0900

    今後のデータ拡張のためにレコード構文を使って書き直した

--- Tapl.hs
+++ Tapl.hs
@@ -14,40 +14,40 @@ trace t | isValue t = (True, [])
                                   in (b, t':ts)
         | otherwise = (False, [])
 
-data Term = TTrue
-          | TFalse
-          | TIf Term Term Term
-          | TZero
-          | TSucc Term
-          | TPred Term
-          | TIsZero Term
-          | TAbs Term
-          | TIndex Int
-          | TApply Term Term
+data Term = TTrue {}
+          | TFalse {}
+          | TIf { cond :: Term, true :: Term, false :: Term }
+          | TZero {}
+          | TSucc { unsucc :: Term }
+          | TPred { unpred :: Term }
+          | TIsZero { zero :: Term }
+          | TAbs { body :: Term }
+          | TIndex { index :: Int }
+          | TApply { func :: Term, arg :: Term }
             deriving (Show, Eq)
 
 instance System Term where
     isValue TTrue = True
     isValue TFalse = True
     isValue TZero = True
-    isValue t@(TSucc _) = isIntValue t
-    isValue t@(TPred _) = isIntValue t
-    isValue (TAbs _) = True
+    isValue t@(TSucc {}) = isIntValue t
+    isValue t@(TPred {}) = isIntValue t
+    isValue (TAbs {}) = True
     isValue _ = False
-    evalStep (TIf TTrue t _) = Just t
-    evalStep (TIf TFalse _ f) = Just f
-    evalStep (TIf c t f) = TIf <$> evalStep c <*> pure t <*> pure f
-    evalStep (TIsZero TZero) = Just TTrue
-    evalStep (TIsZero n) | isIntValue n = Just TFalse
-                         | otherwise = TIsZero <$> evalStep n
-    evalStep (TSucc (TPred n)) | isIntValue n = Just n
-    evalStep (TPred (TSucc n)) | isIntValue n = Just n
-    evalStep (TSucc n) | not $ isValue n = TSucc <$> evalStep n
-    evalStep (TPred n) | not $ isValue n = TPred <$> evalStep n
-    evalStep (TApply t1 t2) | not $ isValue t1 = TApply <$> evalStep t1 <*> pure t2
-                            | not $ isValue t2 = TApply t1 <$> evalStep t2
-                            | (TAbs t') <- t1 = let t'' = substitute t' t2 0
-                                                in Just $ shift t'' 1 (-1)
+    evalStep (TIf { cond = TTrue, true = t }) = Just t
+    evalStep (TIf { cond = TFalse, false = t }) = Just t
+    evalStep (TIf { cond = c, true = t, false = f}) = TIf <$> evalStep c <*> pure t <*> pure f
+    evalStep (TIsZero { zero = TZero }) = Just TTrue
+    evalStep (TIsZero { zero = n }) | isIntValue n = Just TFalse
+                                    | otherwise = TIsZero <$> evalStep n
+    evalStep (TSucc { unsucc = (TPred { unpred = n })}) | isIntValue n = Just n
+    evalStep (TPred { unpred = (TSucc { unsucc = n })}) | isIntValue n = Just n
+    evalStep (TSucc { unsucc = n}) | not $ isValue n = TSucc <$> evalStep n
+    evalStep (TPred { unpred = n}) | not $ isValue n = TPred <$> evalStep n
+    evalStep (TApply { func = t1, arg = t2 }) | not $ isValue t1 = TApply <$> evalStep t1 <*> pure t2
+                                             | not $ isValue t2 = TApply t1 <$> evalStep t2
+                                             | (TAbs { body = t' }) <- t1 = let t'' = substitute t' t2 0
+                                                                            in Just $ shift t'' 1 (-1)
     evalStep _ = Nothing
 
 isIntValue :: Term -> Bool
@@ -59,23 +59,23 @@ isIntValue (TPred t@(TPred _)) = isIntValue $ t
 isIntValue _ = False
 
 substitute :: Term -> Term -> Int -> Term
-substitute (TIndex j) t2 i | j == i = t2
-substitute (TAbs t) t2 i = TAbs $ substitute t (shift t2 1 1) (i+1)
-substitute (TIf c t f) t2 i = TIf (substitute c t2 i) (substitute t t2 i) (substitute f t2 i)
-substitute (TSucc n) t2 i = TSucc $ substitute n t2 i
-substitute (TPred n) t2 i = TPred $ substitute n t2 i
-substitute (TIsZero n) t2 i = TIsZero $ substitute n t2 i
-substitute (TApply t3 t4) t2 i = TApply (substitute t3 t2 i) (substitute t4 t2 i)
+substitute (TIndex { index = j }) t2 i | j == i = t2
+substitute t@(TAbs { body = t' }) t2 i = t { body = substitute t' (shift t2 1 1) (i+1) }
+substitute (TIf { cond = c, true = t, false = f}) t2 i = TIf (substitute c t2 i) (substitute t t2 i) (substitute f t2 i)
+substitute (TSucc { unsucc = n }) t2 i = TSucc $ substitute n t2 i
+substitute (TPred { unpred = n }) t2 i = TPred $ substitute n t2 i
+substitute (TIsZero { zero = n }) t2 i = TIsZero $ substitute n t2 i
+substitute (TApply { func = t3, arg = t4 }) t2 i = TApply (substitute t3 t2 i) (substitute t4 t2 i)
 substitute t1 _ _ = t1
 
 shift :: Term -> Int -> Int -> Term
-shift (TIndex j) i n | j >= i = TIndex $ j + n
-shift (TAbs t) i n = TAbs $ shift t (i + 1) n
-shift (TIf c t f) i n = TIf (shift c i n) (shift t i n) (shift f i n)
-shift (TSucc z) i n = TSucc $ shift z i n
-shift (TPred z) i n = TPred $ shift z i n
-shift (TIsZero z) i n = TIsZero $ shift z i n
-shift (TApply t1 t2) i n = TApply (shift t1 i n) (shift t2 i n)
+shift (TIndex { index = j }) i n | j >= i = TIndex $ j + n
+shift t@(TAbs { body = t' }) i n = t { body = shift t' (i + 1) n }
+shift (TIf { cond = c, true = t, false = f}) i n = TIf (shift c i n) (shift t i n) (shift f i n)
+shift (TSucc { unsucc = z }) i n = TSucc $ shift z i n
+shift (TPred { unpred = z }) i n = TPred $ shift z i n
+shift (TIsZero { zero = z }) i n = TIsZero $ shift z i n
+shift (TApply { func = t1, arg = t2 }) i n = TApply (shift t1 i n) (shift t2 i n)
 shift t _ _ = t
 
 test :: (Eq a, Show a) => a -> a -> IO ()
